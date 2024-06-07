b384bf77665cdcdfb3e30332924e6c0e1f73b30e
chanmaoganda <avaniawang@outlook.com>
2024-06-07 14:52:15 +0800

    need to create a new branch

--- crates/annealers/run.py
+++ crates/annealers/run.py
@@ -1,8 +1,7 @@
 #!/home/avania/projects/python/MathorCupD-2023/.venv/bin/python
+
 import random
-from typing import List
-from JobShopWithArgs import JobShopWithArgs    
-from multiprocessing import Pool
+from JobShopWithArgs import JobShopWithArgs
 import time
 
 from InstanceMaker import InstanceMaker
@@ -14,11 +13,18 @@ def solve_one_instance_one_iteration():
     # TODO: I need to randomly select an instance from the list of instances 
     # TODO: and then select part of them to run and compare the results.
     # TODO: Also, the truck list is intended to be generated randomly, so I need to fix that.
-    random_index = random.randint(0, len(instances)-1)
+    random_index = random.randint(0, len(instances) - 1)
     random_instance = instances[random_index]
     
     instance_solver : Instance = Instance(random_instance, [], instance_maker.data)
+    MAX_ITERATION_NUMBER = 1000
     
+    for i in range(MAX_ITERATION_NUMBER):
+        # TODO: Select a random part of the instance to run and compare the results.
+        sub_instance = instance_solver
+        sub_job_shop = JobShopWithArgs(sub_instance)
+        
+        
     jobshop = JobShopWithArgs(instance_solver)
     jobshop.solve()
     
